033e47844c96614c8339cc2054c7a756748c2eea
AsimanGasimzada <asiman.qasimzade@gmail.com>
2026-01-27 18:18:57 +0400

    Add project files.

---
+++ Pustok.Business/Services/Abstractions/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿namespace Pustok.Business.Services.Abstractions;
+
+public interface ICategoryService
+{
+    Task CreateAsync(CategoryCreateDto dto);
+    Task UpdateAsync(CategoryUpdateDto dto);
+    Task DeleteAsync(Guid id);
+    Task<List<CategoryGetDto>> GetAllAsync();
+    Task<CategoryGetDto> GetByIdAsync(Guid id);
+
+
+    //Task<List<ProductGetDto>> GetProductsByCategoryId(Guid categoryId);
+}
---
+++ Pustok.DataAccess/Contexts/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pustok.Core.Entites;
+using System.Reflection;
+
+namespace Pustok.DataAccess.Contexts;
+
+internal class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
